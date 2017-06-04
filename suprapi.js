bcac994a02e6d660005a5cbb9f95e45813c84749
Josh M. McKee <josh@snazzware.com>
2017-06-04 03:57:00 +0000

    Spaces to tabs, to match upstream

--- suprapi.js
+++ suprapi.js
@@ -25,82 +25,82 @@ const http = require('http');
 */
 
 exports.moduleInfo = {
-    name: 'SupraPiModule',
-    desc: 'SupraPiModule',
-    author: 'snazzware',
-    packageName: 'com.snazzware.suprapimodule'
+	name: 'SupraPiModule',
+	desc: 'SupraPiModule',
+	author: 'snazzware',
+	packageName: 'com.snazzware.suprapimodule'
 };
 
 exports.getModule = class SupraPiModule extends MenuModule {
-    constructor(options) {
-        super(options);
+	constructor(options) {
+		super(options);
 
-        this.config = options.menuConfig.options;
-        console.log(options);
-        console.log('a');
-    }
+		this.config = options.menuConfig.options;
+		console.log(options);
+		console.log('a');
+	}
 
-    mciReady(mciData, cb) {
-        super.mciReady(mciData, err => {
-            if (err) {
-                return cb(err);
-            }
+	mciReady(mciData, cb) {
+		super.mciReady(mciData, err => {
+			if (err) {
+				return cb(err);
+			}
 
-            const self = this;
-            const vc = self.viewControllers.allViews = new ViewController({
-                client: self.client
-            });
-            console.log('b');
-            async.series(
-                [
-                    function notifySupraPiModem(callback) {
-                        var httpOptions = {};
+			const self = this;
+			const vc = self.viewControllers.allViews = new ViewController({
+				client: self.client
+			});
+			console.log('b');
+			async.series(
+				[
+					function notifySupraPiModem(callback) {
+						var httpOptions = {};
 
-                        if (self.config.connect !== undefined) {
-                            httpOptions = {
-                                host: '192.168.1.120',
-                                port: 80,
-                                path: '/?message=CD&led1=110000&led4=110000&netleds=1',
-                                method: 'GET'
-                            };
-                        } else
-                        if (self.config.login !== undefined) {
-                            httpOptions = {
-                                host: '192.168.1.120',
-                                port: 80,
-                                path: '/?message=' + self.client.user.username + '%20%20%20&led1=110000&led4=110000&netleds=1',
-                                method: 'GET'
-                            };
-                        } else {
-                            httpOptions = {
-                                host: '192.168.1.120',
-                                port: 80,
-                                path: '/?matrix=1',
-                                method: 'GET'
-                            };
-                        }
+						if (self.config.connect !== undefined) {
+							httpOptions = {
+								host: '192.168.1.120',
+								port: 80,
+								path: '/?message=CD&led1=110000&led4=110000&netleds=1',
+								method: 'GET'
+							};
+						} else
+						if (self.config.login !== undefined) {
+							httpOptions = {
+								host: '192.168.1.120',
+								port: 80,
+								path: '/?message=' + self.client.user.username + '%20%20%20&led1=110000&led4=110000&netleds=1',
+								method: 'GET'
+							};
+						} else {
+							httpOptions = {
+								host: '192.168.1.120',
+								port: 80,
+								path: '/?matrix=1',
+								method: 'GET'
+							};
+						}
 
-                        console.log(httpOptions);
+						console.log(httpOptions);
 
-                        http.request(httpOptions, function(res) {
-                            res.setEncoding('utf8');
-                            res.on('data', function(chunk) {
-                                console.log('received data');
-                            });
-                        }).end();
+						http.request(httpOptions, function(res) {
+							res.setEncoding('utf8');
+							res.on('data', function(chunk) {
+								console.log('received data');
+							});
+						}).end();
 
-                        return callback(null);
-                    }
-                ],
-                function complete(err) {
-                    if (err) {
-                        self.client.log.error({
-                            error: err.message
-                        }, 'Error loading SupraPiModule');
-                    }
-                    return cb(err);
-                }
-            );
-        });
-    }
+						return callback(null);
+					}
+				],
+				function complete(err) {
+					if (err) {
+						self.client.log.error({
+							error: err.message
+						}, 'Error loading SupraPiModule');
+					}
+					return cb(err);
+				}
+			);
+		});
+	}
 };
