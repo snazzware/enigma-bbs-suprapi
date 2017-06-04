0f07534d091274b518513065022c92d52a13c84e
Josh M. McKee <josh@snazzware.com>
2017-06-04 04:23:27 +0000

    Remove unused code

--- suprapi.js
+++ suprapi.js
@@ -3,10 +3,6 @@
 
 //	ENiGMA½
 const MenuModule = require('../../core/menu_module.js').MenuModule;
-const ViewController = require('../../core/view_controller.js').ViewController;
-const StatLog = require('../../core/stat_log.js');
-const User = require('../../core/user.js');
-const stringFormat = require('../../core/string_format.js');
 
 //	deps
 const http = require('http');
@@ -31,23 +27,18 @@ exports.getModule = class SupraPiModule extends MenuModule {
 				return cb(err);
 			}
 
-			const self = this;
-			const vc = self.viewControllers.allViews = new ViewController({
-				client: self.client
-			});
-
 			var httpOptions = {
-				host: self.config.host,
-				port: self.config.port,
+				host: this.config.host,
+				port: this.config.port,
 				path: '/?message=OK&led1=000000&led4=110000&netleds=1',
 				method: 'GET'
 			};
 
-			if (self.config.connect !== undefined) {
+			if (this.config.connect !== undefined) {
 				httpOptions.path = '/?message=CD&led1=110000&led4=110000&netleds=1';
 			} else
-			if (self.config.login !== undefined) {
-				httpOptions.path = '/?message=' + self.client.user.username + '%20%20%20&led1=110000&led4=110000&netleds=1';
+			if (this.config.login !== undefined) {
+				httpOptions.path = '/?message=' + this.client.user.username + '%20%20%20&led1=110000&led4=110000&netleds=1';
 			} else {
 				httpOptions.path = '/?matrix=1';
 			}
